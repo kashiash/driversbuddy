40d4e47b5fb4a835aecceee47e60e844ad67f1ed
Jacek Kosinski <kashiash@gmail.com>
2024-03-02 13:15:57 +0100

    testowy comit

---
+++ driverbuddy/driverbuddy/driverbuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  driverbuddyApp.swift
+//  driverbuddy
+//
+//  Created by Jacek Kosinski U on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct driverbuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
