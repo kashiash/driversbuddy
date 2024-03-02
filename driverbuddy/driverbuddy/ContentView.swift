7c7626ed8d131f2982c39b264d97cd760fb3f060
Jacek Kosinski <kashiash@gmail.com>
2024-03-02 22:55:22 +0100

    mapview

--- driverbuddy/driverbuddy/ContentView.swift
+++ driverbuddy/driverbuddy/ContentView.swift
@@ -6,28 +6,28 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
 
     @State var showSheet = false
+    @State var activeTab: Tab = .stations
+
     var body: some View {
-        TabView {
-            ForEach(Tab.allCases, id: \.rawValue) {
-                tab in
-                Text(tab.rawValue)
-                    .tag(tab)
-                    .tabItem {
-                        Image(systemName: tab.symbol)
-                        Text(tab.rawValue)
-                    }
-            }
+        ZStack(alignment: .bottom) {
+            Map(initialPosition: .region(.palacKultury))
         }
+            TabBar()
+            .frame(height:49)
+            .background(.regularMaterial)
         .task{
             showSheet = true
         }
         .sheet(isPresented: $showSheet) {
             VStack(alignment: .leading,spacing: 10, content: {
-                Text("Test")
+                Text(activeTab.rawValue)
+                    .font(.title2)
+                    .fontWeight(.semibold)
             })
             .padding()
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
@@ -35,11 +35,43 @@ struct ContentView: View {
             .presentationCornerRadius(20)
             .presentationBackground(.regularMaterial)
             .presentationBackgroundInteraction(.enabled(upThrough: .large))
+            .interactiveDismissDisabled()
             .bottomMaskForSheet()
+        }
+    }
+
+    //Tab Bar
+
+    @ViewBuilder
+    func TabBar() -> some View {
+        HStack(spacing: 0) {
+            ForEach(Tab.allCases, id: \.rawValue) { tab in
+                Button(action: {
+                    activeTab = tab
+                }, label: {
+                    VStack {
+                        Image(systemName: tab.symbol)
+                            .font(.title2)
+                        Text(tab.rawValue)
+                            .font(.caption2)
+                    }
+                    .foregroundStyle(activeTab == tab ? Color.accentColor : .gray)
+                    .frame(maxWidth: .infinity,maxHeight: .infinity)
+                    .contentShape(.rect)
+                })
+                .buttonStyle(.plain)
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+extension MKCoordinateRegion {
+    static var palacKultury: MKCoordinateRegion {
+        let center = CLLocationCoordinate2D(latitude: 52.2315, longitude: 21.0069)
+
+        return .init(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+    }
 }
