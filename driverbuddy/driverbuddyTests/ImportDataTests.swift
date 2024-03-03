69c15c7216c279d3d4ede150952263a9834455dc
Jacek Kosinski <kashiash@gmail.com>
2024-03-04 00:29:07 +0100

    cos jest nie tak ...

--- driverbuddy/driverbuddyTests/ImportDataTests.swift
+++ driverbuddy/driverbuddyTests/ImportDataTests.swift
@@ -18,7 +18,54 @@ final class ImportDataTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+//    func testGetMakes() throws {
+//
+//        let url = URL(string: "https://gist.githubusercontent.com/kashiash/54a66f7171a81bec767e96cdcb8d2212/raw/a0e74a0ec62a17c2741a8a23fb806d48aef5d13f/makes.csv")!
+//
+//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            // Check for errors
+//            if let error = error {
+//                print("Error: \(error)")
+//                return
+//            }
+//
+//            // Check if data is not nil
+//            guard let data = data else {
+//                print("No data received")
+//                return
+//            }
+//
+//            // Convert Data to String
+//            if let resultString = String(data: data, encoding: .utf8) {
+//                print("CSV Content:\n\(resultString)")
+//
+//
+//
+//                let csv: CSV = try CSV<Named>(string: resultString)
+//
+//
+//                print(csv.header)
+//                csv.enumerateAsDict { dict in
+//
+//                    var makeId = dict["'id_car_make'"]?.replacingOccurrences(of: "'", with: "")
+//                    var makeName = dict["'name'"]?.replacingOccurrences(of: "'", with: "")
+//
+//                    print("\(makeId) \(makeName) ")
+//                }
+//
+//
+//            } else {
+//                print("Unable to convert Data to String")
+//            }
+//        }
+//
+//        // Start the URLSession data task
+//        task.resume()
+//
+//    }
+
+
+    func test_getModels() throws {
 
 
         let gistRawURL = "https://gist.githubusercontent.com/kashiash/54a66f7171a81bec767e96cdcb8d2212/raw/a0e74a0ec62a17c2741a8a23fb806d48aef5d13f/makes.csv"
@@ -28,7 +75,7 @@ final class ImportDataTests: XCTestCase {
             do {
                 // Download the CSV file content
                 let csvContent = try String(contentsOf: url)
-             //   print(csvContent)
+                //   print(csvContent)
 
                 do {
                     // As a string, guessing the delimiter
@@ -38,34 +85,25 @@ final class ImportDataTests: XCTestCase {
                     print(csv.header)
                     do {
 
-                         try   csv.enumerateAsDict { dict in
+                        try   csv.enumerateAsDict { dict in
 
-                             var makeId = dict["'id_car_make'"]?.replacingOccurrences(of: "'", with: "")
-                             var makeName = dict["'name'"]?.replacingOccurrences(of: "'", with: "")
+                            var makeId = dict["'id_car_make'"]?.replacingOccurrences(of: "'", with: "")
+                            var makeName = dict["'name'"]?.replacingOccurrences(of: "'", with: "")
 
-                                print("\(makeId) \(makeName) ")
-                            }
-                        } catch {
-                            print(error)
+                            print("\(makeId) \(makeName) ")
                         }
+                    } catch {
+                        print(error)
                     }
-
-//                    csv.enumerateAsArray { array in
-//                        print(array[0])
-//                        print(array[1])
-//                    }
-
-
-
-
-              //  } catch parseError as CSVParseError {
-                    // Catch errors from parsing invalid CSV
-                } catch {
-                   print(error)
                 }
+
+            } catch {
+                print(error)
             }
+        }
 
     }
+
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
