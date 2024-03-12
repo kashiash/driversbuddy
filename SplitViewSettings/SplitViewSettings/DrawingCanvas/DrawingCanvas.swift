509297ab704920dba059aa82d616b6b7b19c33cd
Jacek Kosinski <kashiash@gmail.com>
2024-03-12 18:31:39 +0100

    .

---
+++ SplitViewSettings/SplitViewSettings/DrawingCanvas/DrawingCanvas.swift
@@ -0,0 +1,23 @@
+//
+//  DrawingCanvas.swift
+//  SplitViewSettings
+//
+//  Created by Jacek Kosinski U on 12/03/2024.
+//
+
+import SwiftUI
+
+struct DrawingCanvas: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+
+
+        }
+    }
+}
+
+#Preview {
+    DrawingCanvas()
+}
