30a990ab9d6463c235e08ec001ba40da72943bc6
Jacek Kosinski <kashiash@gmail.com>
2024-03-22 15:04:41 +0100

    photosAlbum

---
+++ PhotoAlbum/PhotoAlbum/PhotoAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAlbumApp.swift
+//  PhotoAlbum
+//
+//  Created by Jacek Kosinski U on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PhotosListView()
+        }
+    }
+}
---
+++ PhotoAlbum/PhotoAlbum/PhotosListView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoAlbum
+//
+//  Created by Jacek Kosinski U on 22/03/2024.
+//
+
+import SwiftUI
+
+struct PhotosListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PhotosListView()
+}
