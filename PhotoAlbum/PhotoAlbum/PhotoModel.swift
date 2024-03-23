cc602a2d4220ac07c105694d878e7ba6faa0f300
Jacek Kosinski <kashiash@gmail.com>
2024-03-23 20:15:54 +0100

    photomodel

--- PhotoAlbum/PhotoAlbum/PhotoModel.swift
+++ PhotoAlbum/PhotoAlbum/PhotoModel.swift
@@ -21,9 +21,30 @@ class PhotoModel {
             return nil
         }
     }
+
+    init(name: String, data: Data? = nil) {
+        self.name = name
+        self.data = data
+    }
 }
 
 extension PhotoModel {
     @MainActor
-    static var preview: ModelContainer
+    static var preview: ModelContainer {
+        let container = try! ModelContainer(
+            for:  PhotoModel.self,
+            configurations: ModelConfiguration(isStoredInMemoryOnly: true)
+        )
+
+        var samples: [PhotoModel] {
+            [
+                .init(name: "Sample 1"), .init(name: "Sample 2"), .init(name: "Sample 3")
+            ]
+        }
+
+        samples.forEach {
+            container.mainContext.insert($0)
+        }
+        return container
+    }
 }
