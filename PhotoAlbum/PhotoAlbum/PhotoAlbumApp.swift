7de0b3ab89f102192d3497841143c7f12732735c
Jacek Kosinski <kashiash@gmail.com>
2024-03-23 22:50:23 +0100

    swipe action

--- PhotoAlbum/PhotoAlbum/PhotoAlbumApp.swift
+++ PhotoAlbum/PhotoAlbum/PhotoAlbumApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct PhotoAlbumApp: App {
@@ -13,5 +14,6 @@ struct PhotoAlbumApp: App {
         WindowGroup {
             PhotosListView()
         }
+        .modelContainer(for: PhotoModel.self)
     }
 }
--- PhotoAlbum/PhotoAlbum/PhotosListView.swift
+++ PhotoAlbum/PhotoAlbum/PhotosListView.swift
@@ -6,19 +6,60 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct PhotosListView: View {
+
+    @Query(sort: \PhotoModel.name) var samples : [PhotoModel]
+    @Environment(\.modelContext) private var modelContext
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Group{
+                if samples.isEmpty {
+                    ContentUnavailableView("Add photo", systemImage: "photo")
+                } else {
+                    List(samples) { sample in
+                        NavigationLink(value: sample) {
+                            HStack{
+                                Image(uiImage: sample.image == nil ? Constants.placeholder : sample.image!)
+                                   .resizable()
+                                   .scaledToFill()
+                                   .frame(width: 50, height: 50)
+                                   .cornerRadius(12)
+                                   .clipped()
+                                   .padding(.trailing)
+                                Text(sample.name)
+                                    .font(.title)
+                            }
+                        }
+                        .swipeActions{
+                            Button(role: .destructive) {
+                                modelContext.delete(sample)
+                                try? modelContext.save()
+                            } label: {
+                                Image(systemName: "trash")
+                            }
+                        }
+                    }
+                    .listStyle(.plain)
+                }
+            }
+            .navigationDestination(for: PhotoModel.self) { sample in
+                Text(sample.name)
+            }
+            .navigationTitle("Picker or camera")
+            .toolbar {
+                Button {
+
+                } label: {
+Image(systemName: "plus.circle.fill")
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     PhotosListView()
+        .modelContainer(PhotoModel.preview)
 }
