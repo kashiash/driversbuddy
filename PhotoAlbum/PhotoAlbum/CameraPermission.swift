a4d0c15036cf105ae2590f9f5b0841172b3ac61d
Jacek Kosinski <kashiash@gmail.com>
2024-03-24 21:29:21 +0100

    camera permission

---
+++ PhotoAlbum/PhotoAlbum/CameraPermission.swift
@@ -0,0 +1,56 @@
+//
+//  CameraPermission.swift
+//  PhotoAlbum
+//
+//  Created by Jacek Kosinski U on 24/03/2024.
+//
+
+import UIKit
+import AVFoundation
+
+enum CameraPermission {
+    enum CameraError: Error, LocalizedError {
+        case unathorized
+        case unavailable
+
+        var errorDescription: String? {
+            switch self {
+
+            case .unathorized:
+                return NSLocalizedString("You have not authorized camera use", comment: "")
+            case .unavailable:
+                return NSLocalizedString("A camera is not available for this device", comment: "")
+            }
+        }
+        var recoverySuggestion: String? {
+            switch self {
+
+            case .unathorized:
+                return "Open Settings > Privacy and Security > Camera and turn on for this app."
+            case .unavailable:
+                return "use photo album instead"
+            }
+        }
+
+        static func checkPermissions() -> CameraError? {
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                let authStatus = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+                switch authStatus {
+                case .notDetermined:
+                    return nil
+                case .restricted:
+                    return nil
+                case .denied:
+                    return .unathorized
+                case .authorized:
+                    return nil
+                @unknown default:
+                    return nil
+                }
+            } else {
+                return .unavailable
+            }
+        }
+    }
+
+}
---
+++ PhotoAlbum/PhotoAlbum/UIKitCamera.swift
@@ -0,0 +1,18 @@
+//
+//  UIKitCamera.swift
+//  PhotoAlbum
+//
+//  Created by Jacek Kosinski U on 24/03/2024.
+//
+
+import SwiftUI
+
+struct UIKitCamera: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIKitCamera()
+}
