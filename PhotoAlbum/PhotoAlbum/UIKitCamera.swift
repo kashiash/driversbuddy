c82386b0edcf7965824d89591daa670953fed028
Jacek Kosinski <kashiash@gmail.com>
2024-03-24 22:38:45 +0100

    UIKitCamera

--- PhotoAlbum/PhotoAlbum/UIKitCamera.swift
+++ PhotoAlbum/PhotoAlbum/UIKitCamera.swift
@@ -5,14 +5,44 @@
 //  Created by Jacek Kosinski U on 24/03/2024.
 //
 
+import UIKit
 import SwiftUI
 
-struct UIKitCamera: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct UIKitCamera : UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.dismiss) var dismiss
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
     }
+    
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+    }
+
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: UIKitCamera
+        init(parent: UIKitCamera) {
+            self.parent =  parent
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.dismiss()
+        }
+
+    }
+
 }
 
-#Preview {
-    UIKitCamera()
-}
+
